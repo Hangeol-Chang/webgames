f9d14979c4d75bc624068505a9d1d978ec76b316
ChangHangeol <hgchang1@naver.com>
2024-12-02 21:32:23 +0900

    타입스크립트 제거

--- next.config.js
+++ next.config.js
@@ -1,22 +1,4 @@
 /** @type {import('next').NextConfig} */
-const debug = process.env.NODE_ENV !== "production";
-const repository = "https://hangeol-chang.github.io/webgames";
-
-const nextConfig = {
-    output: 'export',
-    reactStrictMode: false,
-    assetPrefix: !debug ? `${repository}/` : "/", 
-    trailingSlash: true,
-    
-    env: {
-        BASE_URL: process.env.BASE_URL,
-    },
-    
-    images: {
-        loader: 'akamai',
-        path: '/',
-        unoptimized : true
-    }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
